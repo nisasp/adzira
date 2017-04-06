c61581f3a216306bd4c4c0f0a87e3873c457bbba
adzira <adziranurwidyanto12@gmail.com>
2017-04-06 09:05:40 +0700

    Update No1.java

--- no1/src/no1/No1.java
+++ no1/src/no1/No1.java
@@ -17,12 +17,13 @@ public class No1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       	Scanner a = new Scanner(System.in);
+      Scanner a = new Scanner(System.in);
+	    int hasil = 0 ;
 		System.out.print("Masukkanpa deret fibonacci ? ");
 		int n = a.nextInt();
 		for(int i=1;i<=n;i++)
-			System.out.print(fibo(i) + " ");
-		System.out.println();
+			hasil += fibo(i);
+		System.out.println(hasil);
 	}
 
 	private static int fibo(int n)
